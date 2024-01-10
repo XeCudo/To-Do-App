750fab6198b1320ae850c11dcaaa08099233398a
Zephinax <Zephinax@gmail.com>
2024-01-10 23:47:11 +0330

    Add Dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const inputBox = document.getElementById('inputBox');
 const listContainer = document.getElementById('listContainer');
-
+var checkbox = document.getElementById("theme-checkbox");
 
 function addTask() {
     if (inputBox.value === '') {
@@ -9,11 +9,13 @@ function addTask() {
       let li = document.createElement('li');
       li.innerHTML = '<button class="toggleBtn" onclick="makeChecked(event)"></button><p>' + inputBox.value + '</p><button class="deleteTask" onclick="deleteTask(event)"></button>';
       li.classList.add("fade-in");
+      li.classList.add("listItem");
+      li.id = "listItem";
       listContainer.appendChild(li);
       inputBox.value = '';
       saveData();
     }
-  }
+  };
 
 function makeChecked(event){
     var button = event.target;
@@ -28,25 +30,52 @@ function makeChecked(event){
         button.classList.remove("toggleBtnChecked");
         saveData();
       }
-}
+};
 
 function deleteTask(event) {
   var button = event.target;
   var listItem = button.parentElement;
   listItem.classList.remove('fade-in')
-  listItem.classList.add('fade-out'); // Add 'fade-out' class to the list item
+  listItem.classList.add('fade-out');
   setTimeout(function() {
     listItem.parentNode.removeChild(listItem);
     saveData();
-  }, 300); // Delay the removal of the list item for 300ms (0.3 seconds)
-}
+  }, 300); 
+};
+
+// submit with enter 
 
 document.getElementById("inputBox").addEventListener("keydown", function(event) {
     if (event.keyCode === 13) {
-      event.preventDefault(); // Prevent the default form submission
-      document.getElementById("addBtn").click(); // Trigger the click event of the add button
+      event.preventDefault(); 
+      document.getElementById("addBtn").click(); 
     }
   });
+
+// dark mode enable
+
+function toggleTheme() {
+const todoApp = document.getElementById("todoApp");
+var ul = document.querySelector("ul");
+var liElements = ul.querySelectorAll("li");
+
+if (checkbox.checked) {
+  todoApp.classList.add("todoAppDark")
+  inputBox.classList.add("taskInputDark")
+  liElements.forEach(function(li) {
+    li.classList.add("listItemDark");
+  });
+
+} else {
+  todoApp.classList.remove("todoAppDark")
+  inputBox.classList.remove("taskInputDark")
+  liElements.forEach(function(li) {
+    li.classList.remove("listItemDark");
+  });
+}
+};
+
+// save data 
 
 function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
